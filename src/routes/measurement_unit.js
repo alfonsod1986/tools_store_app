73b2ef874527d51da3bf7b15bd5cb3347ceb029c
Alfonso Delgado <adelgado@gstrackme.com>
2018-06-18 16:44:59 -0500

    Measurement Units v1

--- src/routes/measurement_unit.js
+++ src/routes/measurement_unit.js
@@ -3,7 +3,7 @@
 const router = require('express').Router();
 const measurement_units = require('../controller/measurement_unit');
 
-router.get('/users/', measurement_units.all);
+router.get('/measurement_units/', measurement_units.all);
 
 /* Export module */
 module.exports = router;
